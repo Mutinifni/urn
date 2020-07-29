dbf07d78eada55f449509aacbe6bf0b50f763ffa
Sven Suursoho <sven@alt.ee>
2020-07-29 17:17:46 +0300

    Drop unused error and endpoint

--- urn/list.cmake
+++ urn/list.cmake
@@ -1,10 +1,6 @@
 list(APPEND urn_sources
   urn/__bits/lib.hpp
   urn/__bits/platform_sdk.hpp
-  urn/endpoint_v4.hpp
-  urn/endpoint_v4.cpp
-  urn/error.hpp
-  urn/error.cpp
   urn/mutex.hpp
   urn/relay.hpp
 )
@@ -12,8 +8,6 @@ list(APPEND urn_sources
 list(APPEND urn_unittests_sources
   urn/common.test.hpp
   urn/common.test.cpp
-  urn/endpoint_v4.test.cpp
-  urn/error.test.cpp
   urn/mutex.test.cpp
   urn/relay.test.cpp
 )
