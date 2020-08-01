717cecdea64cb63c9135d1d19ef87252181ffbb9
Sven Suursoho <sven@alt.ee>
2020-08-01 09:29:15 +0300

    intrusive_stack

--- urn/list.cmake
+++ urn/list.cmake
@@ -1,6 +1,7 @@
 list(APPEND urn_sources
   urn/__bits/lib.hpp
   urn/__bits/platform_sdk.hpp
+  urn/intrusive_stack.hpp
   urn/mutex.hpp
   urn/relay.hpp
 )
@@ -8,6 +9,7 @@ list(APPEND urn_sources
 list(APPEND urn_unittests_sources
   urn/common.test.hpp
   urn/common.test.cpp
+  urn/intrusive_stack.test.cpp
   urn/mutex.test.cpp
   urn/relay.test.cpp
 )
